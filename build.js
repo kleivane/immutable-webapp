80c63dbb27df419f09f5febc572c0221161f4dcb
Tine Kleivane <kleivane@gmail.com>
2020-01-30 09:27:26 +0100

    Vis byggetidspunkt i js
    For å vise noe som refererer til compile-time-creation av js

--- build.js
+++ build.js
@@ -1,4 +1,7 @@
+const moment = require('moment');
 const fs = require('fs-extra')
 
+const timestamp = moment().format('dddd, MMMM Do YYYY, hh:mm:ss');
 fs.emptyDirSync('build')
-fs.copySync('src', 'build')
+fs.appendFileSync('build/main.js', `const timestamp = "${timestamp}";`, 'utf8');
+fs.appendFileSync('build/main.js', fs.readFileSync('src/main.js'));
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 const heading = document.createElement("h1");
-const content = document.createTextNode(`Created at ${timestamp}`)
+const content = document.createTextNode(`Build created at ${timestamp}`)
 heading.appendChild(content);
 document.body.appendChild(heading);
