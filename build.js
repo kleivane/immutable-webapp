c5a60f7ec07f3a752e583477da919098810ea39d
Tine Kleivane <tine.kleivane@bekk.no>
2020-02-03 13:06:38 +0100

    Legg til tidssone

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 const moment = require('moment');
 const fs = require('fs-extra')
 
-const timestamp = moment().format('dddd, MMMM Do YYYY, hh:mm:ss');
+const timestamp = moment().format('dddd, MMMM Do YYYY, hh:mm:ss Z');
 fs.emptyDirSync('build')
 fs.appendFileSync('build/main.js', `const timestamp = "${timestamp}";`, 'utf8');
 fs.appendFileSync('build/main.js', fs.readFileSync('src/main.js'));
