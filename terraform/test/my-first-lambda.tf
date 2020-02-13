3330c51c8a12a2e772c3039d87f17c24849cd301
Tine Kleivane <tine.kleivane@bekk.no>
2020-02-13 12:36:39 +0100

    Første lambda-test

---
+++ terraform/test/my-first-lambda.tf
@@ -0,0 +1,41 @@
+variable "app_version" {
+}
+
+resource "aws_lambda_function" "deployer" {
+  function_name = "deploy_test"
+
+  # The bucket name as created earlier with "aws s3api create-bucket"
+  s3_bucket = "immutable-webapp-deploy-src"
+  s3_key    = "v${var.app_version}/src.zip"
+
+  # "main" is the filename within the zip file (main.js) and "handler"
+  # is the name of the property under which the handler function was
+  # exported in that file.
+  handler = "main.handler"
+  runtime = "nodejs10.x"
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+# IAM role which dictates what other AWS services the Lambda function
+# may access.
+resource "aws_iam_role" "lambda_exec" {
+  name = "deployer_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
