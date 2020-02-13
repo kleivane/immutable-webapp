700d590c3c2456465b9beb1f8c49f1a1d00fb1ce
Tine Kleivane <tine.kleivane@bekk.no>
2020-02-13 14:39:04 +0100

    Forventer et objet med sha inn som event til lambdaen

--- terraform/test/src/main.js
+++ terraform/test/src/main.js
@@ -28,8 +28,9 @@ exports.handler = function(event, context, callback) {
     body: '<p>Hello world! 2</p>'
   }
 
-  let environment = process.env.TF_ENVIRONMENT;
-  let url = process.env.TF_API_URL;
+  const environment = process.env.TF_ENVIRONMENT;
+  const url = `https://${process.env.TF_API_URL}`;
+  const sha = event.sha;
 
   const index = `<!doctype html>
   <html>
@@ -45,7 +46,7 @@ exports.handler = function(event, context, callback) {
          <script>
          env = {
              ENV_NAME: '${environment}',
-             GIT_SHA: 'en-sha',
+             GIT_SHA: '${sha}',
              API_URL: '${url}'
          }
          </script>
@@ -54,7 +55,7 @@ exports.handler = function(event, context, callback) {
          <app-root></app-root>
          <h3>Deployed at TIMESTAMP_PLACEHOLDER</h3>
          <!-- fully-qualified static assets -->
-         <script src="${url}/assets/GIT_SHA_PLACEHOLDER/main.js" type="text/javascript"></script>
+         <script src="${url}/assets/${sha}/main.js" type="text/javascript"></script>
 
 
      </body>
