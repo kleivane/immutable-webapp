21e4e6d72db87c9485a19185e43ace9539efc3d9
Tine Kleivane <tine.kleivane@bekk.no>
2020-02-18 10:58:00 +0100

    Legg til www.skysett.net som prod-adresse

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 
 locals {
   environment = "prod"
+  url = "www.skysett.net"
 }
 
 resource "aws_s3_bucket" "prod" {
@@ -17,12 +18,29 @@ resource "aws_s3_bucket" "prod" {
   }
 }
 
+
+
 module "immutable_cloudfront" {
-  source = "git@github.com:kleivane/terraform-aws-cloudfront-s3-assets.git?ref=0.1.0"
+  source = "git@github.com:kleivane/terraform-aws-cloudfront-s3-assets.git?ref=0.3.0"
 
   bucket_origin_id = "S3-${aws_s3_bucket.prod.id}"
   bucket_domain_name = aws_s3_bucket.prod.bucket_regional_domain_name
   environment= local.environment
+
+  aliases = [local.url]
+}
+
+data "aws_route53_zone" "primary" {
+  name         = "skysett.net."
+}
+
+module "production_www" {
+  source = "git::https://github.com/cloudposse/terraform-aws-route53-alias.git?ref=0.5.0"
+  aliases = [local.url]
+  ipv6_enabled = true
+  parent_zone_id = data.aws_route53_zone.primary.zone_id
+  target_dns_name = module.immutable_cloudfront.distribution.domain_name
+  target_zone_id = module.immutable_cloudfront.distribution.hosted_zone_id
 }
 
 module "deployer" {
